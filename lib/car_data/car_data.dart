d40dee8b882be041639d8178c32d70a59d707c51
Jangjun <daki403.1@gmail.com>
2023-11-28 18:28:50 +0900

    refactor(car_data): tag_enum 삭제 및 사용처 정리
    tag_enum 구현을 안할 것이기 떄문에
    그냥 죽어버렸습니다.

--- lib/car_data/car_data.dart
+++ lib/car_data/car_data.dart
@@ -1,51 +1,24 @@
 import 'package:flutter/material.dart';
 
 import 'package:socar/car_data/car.dart';
-import 'package:socar/car_data/tag_enum.dart';
 
 class CarData {
   final Car car;
-  final List<TagEnum> tags;
   final int originFee;
   final int discountRate;
 
   CarData({
     required this.car,
-    required this.tags,
     required this.originFee,
     required this.discountRate,
   });
 }
 
 List<CarData> dummyData = [
-  CarData(
-      car: dummyCars["지호"]!,
-      tags: [TagEnum.NewCar, TagEnum.Recommand],
-      originFee: 10000,
-      discountRate: 10),
-  CarData(
-      car: dummyCars["민석"]!,
-      tags: [TagEnum.NewCar, TagEnum.Recommand],
-      originFee: 15000,
-      discountRate: 20),
-  CarData(
-      car: dummyCars["진영"]!,
-      tags: [TagEnum.NewCar, TagEnum.Recommand],
-      originFee: 20000,
-      discountRate: 30),
-  CarData(
-      car: dummyCars["지호"]!,
-      tags: [TagEnum.NewCar, TagEnum.Recommand],
-      originFee: 10000,
-      discountRate: 10),
-  CarData(
-      car: dummyCars["민석"]!,
-      tags: [TagEnum.NewCar, TagEnum.Recommand],
-      originFee: 15000,
-      discountRate: 20),
-  CarData(
-      car: dummyCars["진영"]!,
-      tags: [TagEnum.NewCar, TagEnum.Recommand],
-      originFee: 20000,
-      discountRate: 30)
+  CarData(car: dummyCars["지호"]!, originFee: 10000, discountRate: 10),
+  CarData(car: dummyCars["민석"]!, originFee: 15000, discountRate: 20),
+  CarData(car: dummyCars["진영"]!, originFee: 20000, discountRate: 30),
+  CarData(car: dummyCars["지호"]!, originFee: 10000, discountRate: 10),
+  CarData(car: dummyCars["민석"]!, originFee: 15000, discountRate: 20),
+  CarData(car: dummyCars["진영"]!, originFee: 20000, discountRate: 30)
 ];
